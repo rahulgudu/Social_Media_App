de07b87d8b80cb8403f965e6101ee23b8c86527b
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-11 14:04:32 +0530

    colour, theme, styling, dark mode setup and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage/index";
+import LoginPage from "./scenes/loginPage/index";
+import ProfilePage from "./scenes/profilePage/index";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 function App() {
-  return <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
-    </BrowserRouter>
-  </div>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,10 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navabar";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return <Box>
+    <Navbar />
+  </Box>;
 };
 
 export default HomePage;
