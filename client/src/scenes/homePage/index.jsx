d8df3a48e15a7591b47dd40dd083226d710a724d
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-13 21:33:09 +0530

    added the add or create post widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,31 +1,37 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navabar";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
-  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
       <Box
         width="100%"
         padding="2rem 6%"
-        display={isNonMobileScreen ? "flex" : "block"}
+        display={isNonMobileScreens ? "flex" : "block"}
         gap="0.5rem"
         justifyContent="space-between"
       >
-        <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
-          <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
-            <UserWidget userId={_id} picturePath={picturePath} />
-          </Box>
-          <Box
-            flexBasis={isNonMobileScreen ? "42%" : undefined}
-            mt={isNonMobileScreen ? undefined : "2rem"}
-          ></Box>
-          {isNonMobileScreen && <Box flexBasis="26%"></Box>}
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+          </Box>
+        )}
       </Box>
     </Box>
   );
